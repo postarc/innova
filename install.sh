1df18266af41eed4b55084f2f1098f889e1ab1dd
postarc <45208608+postarc@users.noreply.github.com>
2018-11-20 21:48:33 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,8 +65,7 @@ crontab tempcron
 
 rm tempcron
 rm -rf ~/innova
-#sudo chown -R $USER:$USER ~/
-#cd ~/innova
+innovad -daemon
 echo "VPS ip: $NODEIP"
 echo "Masternode private key: $MASTERNODEKEY"
 echo "Job completed successfully"
