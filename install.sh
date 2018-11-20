181a7cc13c5fce9b8c76eb26370d9d7ad0b1e11b
postarc <45208608+postarc@users.noreply.github.com>
2018-11-20 20:36:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,20 +11,26 @@ cd ~/innova
 wget $COIN
 tar xvzf linux_x64.tar.gz
 if [ ! -f "/usr/local/bin/innovad" ]; then
+	echo "Copying bin files...."
 	cp innova* /usr/local/bin
+else
+	echo "Bin files exist. Skipping copy..."
 fi
 
 sudo apt-get install -y pwgen
 
 # writing innova.conf file:
+echo "Writing innova config file..."
 while [ -n "$(lsof -i -s TCP:LISTEN -P -n | grep $RPCPORT)" ]
 do
 (( RPCPORT--))
 done
+echo "Free RPCPORT address:$RPCPORT"
 while [ -n "$(lsof -i -s TCP:LISTEN -P -n | grep $PORT)" ]
 do
 (( PORT++))
 done
+echo "Free MN port address:$PORT"
 NODEIP=$(curl -s4 icanhazip.com)
 GEN_PASS=`pwgen -1 20 -n`
 echo -e "rpcuser=innovauser\nrpcpassword=${GEN_PASS}\nrpcport=$RPCPORT\nexternalip=$NODEIP:14520\nport=$PORT\nlisten=1\nmaxconnections=256" > ~/.innovacore/innova.conf
@@ -36,9 +42,9 @@ sleep 10
 MASTERNODEKEY=$(./innova-cli masternode genkey)
 echo -e "masternode=1\nmasternodeprivkey=$MASTERNODEKEY" >> ~/.innovacore/innova.conf
 innova-cli stop
-#rm innova*
 
 # installing SENTINEL
+echo "Start Sentinel installing process..."
 cd ~/.innovacore
 sudo apt-get install -y git python-virtualenv
 sudo git clone https://github.com/innovacoin/sentinel.git
@@ -58,12 +64,14 @@ echo "@reboot /usr/local/bin/innovad -daemon"
 echo "* * * * * cd ~/.innovacore/sentinel && ./venv/bin/python bin/sentinel.py 2>&1 >> sentinel-cron.log" > tempcron
 echo "*/30 * * * * ~/mnchecker/mnchecker --currency-handle=\"innova\" --currency-bin-cli=\"innova-cli\" --currency-datadir=\"~/.innovacore\" > ~/mnchecker/mnchecker-cron.log 2>&1" >> tempcron
 crontab tempcron
+
 rm tempcron
+rm -rf ~/innova
 
 #sudo chown -R $USER:$USER ~/
 #cd ~/innova
 #./innovad -daemon
-#clear
+clear
 echo "VPS ip: $NODEIP"
 echo "Masternode private key: $masternodekey"
 echo "Job completed successfully" 
